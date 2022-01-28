687fb278e7d8d7ff1a55c03edf1de41a976eac39
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-28 16:39:28 -0300

    styles, controlers rooms and alert

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+  resources :message, only: %i[:create]
+
+  resources :rooms
+  
+  get '/search', to: 'rooms#search', as: 'search_rooms'
+
 end
