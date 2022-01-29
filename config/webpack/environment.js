89d7ed8487c4fd2ad6e1a25042c0b87746e291dc
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2022-01-29 20:37:58 -0300

    corrigindo erro do bootstrap && jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,10 @@
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker')
 
-environment.plugins.append('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
